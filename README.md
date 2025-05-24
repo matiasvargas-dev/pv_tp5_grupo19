3470e52053076f9f87b4e41842b64cb21d2ecfdc
Martinezcabj12 <martinezcabj12@gmail.com>
2025-05-24 18:58:01 -0300

    React + Biome

---
+++ README.md
@@ -0,0 +1,1 @@
+# Trabajo Practico N5 - Programación Visual
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+
+import "./App.css";
+
+function App() {
+	return (
+		<>
+		  <h1 className="title">My App</h1>
+		</>
+	);
+}
+
+export default App;
