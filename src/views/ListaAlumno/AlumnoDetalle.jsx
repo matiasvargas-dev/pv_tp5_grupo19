633744ca154b8eef1f3de016ebd61fde260a0115
matiasvargas-dev <42268654@fi.unju.edu.ar>
2025-05-30 17:55:06 -0300

    feat: mejora estilos en AlumnoDetalle con MIU y agrega botón para volver a la lista

--- src/views/ListaAlumno/AlumnoDetalle.jsx
+++ src/views/ListaAlumno/AlumnoDetalle.jsx
@@ -1,32 +1,48 @@
-import { useParams } from "react-router";
+import { useParams, Link } from "react-router";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 import "./style.css";
+
 const AlumnoDetalle = ({ alumnos }) => {
-  const { id } = useParams(); // Extraemos el id del parámetro de la URL
+  const { id } = useParams();
   const alumno = alumnos.find((a) => a.id === parseInt(id));
 
   if (!alumno) return <p>Alumno no encontrado</p>;
 
   return (
-    <div>
-      <h2>
-        Detalle de {alumno.nombre} {alumno.apellido}
-      </h2>
-      <p>
-        <strong>LU:</strong> {alumno.lu}
-      </p>
-      <p>
-        <strong>Curso:</strong> {alumno.curso}
-      </p>
-      <p>
-        <strong>Email:</strong> {alumno.email}
-      </p>
-      <p>
-        <strong>Domicilio:</strong> {alumno.domicilio || "No especificado"}
-      </p>
-      <p>
-        <strong>Teléfono:</strong> {alumno.telefono || "No especificado"}
-      </p>
-    </div>
+    <Card sx={{ maxWidth: 600, margin: "2rem auto", p: 2}}>
+      <CardContent>
+        <Typography variant="h5" component="div" gutterBottom>
+          Detalle de <strong>{alumno.nombre} {alumno.apellido}</strong>
+        </Typography>
+        <Typography variant="body1">
+          <strong>LU:</strong> {alumno.lu}
+        </Typography>
+        <Typography variant="body1">
+          <strong>Curso:</strong> {alumno.curso}
+        </Typography>
+        <Typography variant="body1">
+          <strong>Email:</strong> {alumno.email}
+        </Typography>
+        <Typography variant="body1">
+          <strong>Domicilio:</strong> {alumno.domicilio || "No especificado"}
+        </Typography>
+        <Typography variant="body1">
+          <strong>Teléfono:</strong> {alumno.telefono || "No especificado"}
+        </Typography>
+        <Button
+          component={Link}
+          to="/lista-alumnos"
+          size="small"
+          color="primary"
+          sx={{ mt: 2 }}
+        >
+          Volver a la lista
+        </Button>
+      </CardContent>
+    </Card>
   );
 };
 
