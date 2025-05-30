311309ba4302b57c908062432b26cbf1e4d0c8f2
matiasvargas-dev <42268654@fi.unju.edu.ar>
2025-05-29 23:30:35 -0300

    feat: agrega carpeta data con script de almumnosIniciales y deja import listo en App.jsx

---
+++ src/data/alumnos.js
@@ -0,0 +1,45 @@
+// src/data/alumnos.js
+const alumnos = [
+  {
+    id: 1,
+    lu: 'APU001',
+    nombre: 'Juan',
+    apellido: 'Pérez',
+    curso: 'Tercero',
+    email: 'juan@mail.com',
+    domicilio: 'Avenida Siempre Viva 742',
+    telefono: '987654321'
+  },
+  {
+    id: 2,
+    lu: 'APU002',
+    nombre: 'Ana',
+    apellido: 'Gómez',
+    curso: 'Segundo',
+    email: 'ana@mail.com',
+    domicilio: 'Calle Falsa 123',
+    telefono: '123456789'
+  },
+  {
+    id: 3,
+    lu: 'APU003',
+    nombre: 'Luis',
+    apellido: 'Martínez',
+    curso: 'Primero',
+    email: 'luis@mail.com',
+    domicilio: 'Calle Verdadera 456',
+    telefono: '987654321'
+  },
+  {
+    id: 4,
+    lu: 'APU004',
+    nombre: 'María',
+    apellido: 'López',
+    curso: 'Cuarto',
+    email: 'maria@mail.com',
+    domicilio: 'Calle Falsa 789',
+    telefono: '123123123'
+  },
+];
+
+export default alumnos;
