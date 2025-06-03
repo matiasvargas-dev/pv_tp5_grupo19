2c36461ac38f993c9e11056d6c13d2ebbeaff591
BarroBrisa <brisa.barr022@gmail.com>
2025-06-03 20:28:05 -0300

    feat: reestructura la vista de presentación con un diseño mejorado y navegación a 'AcercaDe'

--- src/views/Home/component/Presentation.jsx
+++ src/views/Home/component/Presentation.jsx
@@ -1,20 +1,75 @@
-import "./Presentation.css";
+import {useNavigate} from "react-router-dom";
+import HomeLayout from "../Layout/HomeLayout";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+
 const Presentation = () => {
+  const navigate = useNavigate();
   return (
-    <section className="presentation-container">
-      <h1 className="presentation-title">
-        Bienvenido a la App de Gestion de Alumno
-      </h1>
-      <p className="presentation-subtitle">
-        Facultad de Ingeneria - Programacion Visual 2025
-      </p>
-      <p className="presentation-text">
-        Esta aplicación está diseñada para facilitar la gestión de alumnos,
-        permitiendo a los docentes llevar un control eficiente de la información
-        académica y personal de los estudiantes.
-      </p>
-    </section>
+    <HomeLayout>
+      <Paper elevation={3} 
+      sx={{
+        background: "linear-gradient(135deg, #ffa07a 0%, #cd5c5c 100%)"
+        borderRadius: 4,
+        p: { xs: 2, sm: 4 },
+        m: { xs: 2, sm: 4 },
+        maxWidth: 600,
+        mx: "auto",
+      }}
+      >
+         <Typography
+          variant="h3"
+          sx={{
+            color: "#34495e",
+            fontWeight: 700,
+            mb: 2,
+            fontSize: { xs: "1.4rem", sm: "2rem" },
+          }}
+     
+      align="center"
+>
+  Bienvenido a la Aplicación de Gestión de Alumnos
+         </Typography>
+   <Typography
+     variant="h5"
+      sx={{
+      color: "#2c3e50",
+      fontWeight: 500,
+      mt: 2,
+      mb: 1,
+      fontSize: { xs: "1.1rem", sm: "1.25rem" },
+      }}
+     align="center"
+        >
+      Facultad de Ingeneria - Programacion Visual 2025
+    </Typography>
+     <Typography
+          variant="body1"
+          sx={{
+            color: "#34495e",
+            fontSize: { xs: "1rem", sm: "1.2rem" },
+            mb: 3,
+          }}
+          align="center"
+          >
+   Esta aplicación fue desarrollada por el Grupo 19 para el trabajo practico n°5 fue diseñada para facilitar 
+   la gestión de alumnos, permitiendo a los docentes llevar un control eficiente de la información académica y personal de los estudiantes.
+    </Typography>  
+     <Box display="flex" justifyContent="center">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => navigate("/acercade")}
+            sx={{ borderRadius: 2, px: 4, py: 1 }}
+          >
+            Ver Alumnos
+          </Button>
+        </Box>
+</Paper>
+    </HomeLayout>
   );
 };
-
 export default Presentation;
+
