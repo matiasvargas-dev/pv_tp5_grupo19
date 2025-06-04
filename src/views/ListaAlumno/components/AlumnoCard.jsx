9d4424fcb7bc9a56bd1c891de5fec2a749a6b02d
matiasvargas-dev <42268654@fi.unju.edu.ar>
2025-06-03 21:30:23 -0300

    feat: mostrar LU en cursiva y fecha de creacion en AlumnoCard y AlumnoDetalles

--- src/views/ListaAlumno/components/AlumnoCard.jsx
+++ src/views/ListaAlumno/components/AlumnoCard.jsx
@@ -6,15 +6,21 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 
 const AlumnoCard = ({ alumno, onDelete, onEditar }) => {
-  const { id, nombre, apellido, curso } = alumno;
+  const { id, nombre, apellido, curso, lu } = alumno;
   return (
     <Card sx={{ mb: 2 }}>
       <CardContent>
+        <Typography variant="subtitle2" component="div" color="text.secondary">
+          <em>{lu}</em>
+        </Typography> 
         <Typography variant="h6" component="div">
           {nombre} {apellido}
         </Typography>
         <Typography color="text.secondary">
-          <strong>Curso:</strong> {curso}
+          Curso: {curso}
+        </Typography>
+        <Typography color="text.secondary">
+         Fecha de creación: {new Date(id).toLocaleDateString('es-AR')} {new Date(id).toLocaleTimeString('es-AR', { hour: '2-digit', minute: '2-digit' })}
         </Typography>
       </CardContent>
       <CardActions>
